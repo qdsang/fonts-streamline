f3686e69f2f415e1d3bbf8f6a51c64c51ad7a535
fenglong <fenglong@xiaomi.com>
2014-07-24 00:49:22 +0800

    添加同时生成woff ttf eot格式文件

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ fonts-streamline
 
 提取想要的字体，提高字体文件利用率
 
-### 使用方法
+## 使用方法
 
 > fonts-streamline 文本 ./svg字体文件.svg ./输出字体文件.svg
 
@@ -13,6 +13,48 @@ fonts-streamline
 eg:
 fonts-streamline "一块钢板的艺术之旅40道工艺制程，193道精细工序1010步锻压成型不锈钢金属边框，40个工艺制程。 依然是全球最快的手机。小米手机 31234567890" ./FZLTCXHJW.svg ./iconfont.svg
 ```
+
+# 关于字体文件格式转换成SVG格式
+
+## 安装必要组件
+
+### OS X
+
+```
+brew install ttfautohint fontforge --with-python
+```
+
+*You may need to use `sudo` for `brew`, depending on your setup.*
+
+
+### Linux
+
+```
+sudo apt-get install fontforge ttfautohint
+```
+
+
+### Windows
+
+```
+npm install grunt-webfont --save-dev
+```
+
+Then [install `ttfautohint`](http://www.freetype.org/ttfautohint/#download) (optional).
+
+*Only `node` engine available (see below).*
+
+
+### 参考 
+
+https://github.com/sapegin/grunt-webfont
+
+
+## 开始转换
+
+###使用脚本 convert2svgfont.pe
+
+### eg: ./convert2svgfont.pe FZLTCXHJW.TTF
 
 
 ### 参考项目
--- bin/fonts-streamline.js
+++ bin/fonts-streamline.js
@@ -1,24 +1,30 @@
 #! /usr/bin/env node
 
 var svgfont2svgicons = require(__dirname + '/../src/svgfont2svgicons.js')
-  , Fs = require('fs')
+  , fs = require('fs')
   , path = require('path')
 ;
-
+var StringDecoder = require('string_decoder').StringDecoder;
 var svgicons2svgfont = require(__dirname + '/../src/svgicons2svgfont.js');
+
+var svg2ttf = require('svg2ttf');
+var ttf2woff = require('ttf2woff');
+var ttf2eot = require('ttf2eot');
 
 var fontString = process.argv[2];
 var fontFileInput = process.argv[3];
 var fontFileOutput = process.argv[4];
-var fontFileOutputName = process.argv[5];
+var fontFileOutputFileName = path.basename(fontFileOutput, path.extname(fontFileOutput));
+var fontFileOutput_fix = path.join(path.dirname(fontFileOutput), fontFileOutputFileName);
+var fontFileOutputName = process.argv[5] || 'iconfont';
 
-var fontStream = Fs.createReadStream(fontFileInput);
+var fontStream = fs.createReadStream(fontFileInput);
 var iconProvider = svgfont2svgicons();
 var unamedIconCount = 0;
 
 fontStream.pipe(iconProvider);
 
-var glyphs = [], index = 1, timer;
+var glyphs = [], index = 1;
 iconProvider.on('readable', function() {
   var glyph, steam;
   while(null !== glyph) {
@@ -31,18 +37,87 @@ iconProvider.on('readable', function() {
       glyphs.push(glyph);
     }
   }
-  clearTimeout(timer);
-  timer = setTimeout(function(){
-    if (glyphs.length) {
-      makesvgfont(glyphs);
-    }
-  }, 800);
+});
+
+iconProvider.on('end', function() {
+  console.log('svgfont2svgicons end');
+  makesvgfont(glyphs);
 });
 
 function makesvgfont(glyphs){
   //console.log(glyphs);
-  svgicons2svgfont(glyphs, {
-      fontName: fontFileOutputName || ''
-    }
-  ).pipe(Fs.createWriteStream(fontFileOutput));
+  generators.svg(glyphs, function(svgFont){
+
+    console.log('generators svg end');
+    generators.ttf(svgFont, function(ttfFont){
+
+      console.log('generators ttf end');
+      generators.woff(ttfFont, function(){
+
+        console.log('generators woff end');
+        generators.eot(ttfFont, function(){
+          console.log('generators eot end');
+        });
+
+      });
+
+    });
+
+  });
 }
+
+var fonts = {};
+
+var generators = {
+  svg: function(glyphs, done) {
+    // https://github.com/sapegin/grunt-webfont/blob/master/tasks/engines/node.js
+    var font = '';
+    var decoder = new StringDecoder('utf8');
+    var stream = svgicons2svgfont(glyphs, {
+        fontName: fontFileOutputName || ''
+        /*
+        fontName: o.fontName,
+        fontHeight: o.fontHeight,
+        descent: o.descent,
+        normalize: o.normalize,
+        log: logger.verbose.bind(logger),
+        error: logger.error.bind(logger)
+        */
+      }
+    );
+
+    stream.on('data', function(chunk) {
+      font += decoder.write(chunk);
+    });
+
+    stream.on('end', function() {
+      fonts.svg = font;
+      fs.writeFileSync(fontFileOutput_fix + '.svg', font);
+      done(font);
+    });
+  },
+  ttf: function(svgFont, done) {
+    var ttf = svg2ttf(svgFont, {});
+    var font = new Buffer(ttf.buffer);
+    fonts.ttf = font;
+    fs.writeFileSync(fontFileOutput_fix + '.ttf', font);
+    done(font);
+  },
+
+  woff: function(ttfFont, done) {
+    var font = ttf2woff(new Uint8Array(ttfFont), {});
+    font = new Buffer(font.buffer);
+    fonts.woff = font;
+    fs.writeFileSync(fontFileOutput_fix + '.woff', font);
+    done(font);
+  },
+
+  eot: function(ttfFont, done) {
+    var font = ttf2eot(new Uint8Array(ttfFont));
+    font = new Buffer(font.buffer);
+    fonts.eot = font;
+    fs.writeFileSync(fontFileOutput_fix + '.eot', font);
+    done(font);
+  }
+}
+
